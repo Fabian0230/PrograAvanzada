46d9e18e1d87e1033c5a683592a8109867de4afc
soportetecnicolatina <soportetecnicolatina@outlook.es>
2017-05-29 20:25:38 -0600

    Se agrega proyecto de logica de negocio

---
+++ Ulatina.PrograAvanzada.01/Ulatina.PrograAvanzada.BLAplicacionWeb/Dominio/Acciones/ObtenerFechaActual.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.PrograAvanzada.BLAplicacionWeb.Dominio.Acciones
+{
+    public class ObtenerFechaActual
+    {
+        //Accion
+        public string ObtengaLaFecha()
+        {
+            //Formas de declarar un objeto
+            //Especificaciones.ObtengaFecha laEspecificacion = new Especificaciones.ObtengaFecha();
+            var laEspecificacion = new Especificaciones.ObtengaFecha();//indica que va a recibir una variable del tipo clase
+            string laFecha = laEspecificacion.FormatearUnaFecha(DateTime.Now, "dd/mm/yyyy");
+            return laFecha;
+        }
+    }
+}
---
+++ Ulatina.PrograAvanzada.01/Ulatina.PrograAvanzada.BLAplicacionWeb/Dominio/Especificaciones/ObtengaFecha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ulatina.PrograAvanzada.BLAplicacionWeb.Dominio.Especificaciones
+{
+    public class ObtengaFecha
+    {
+        //Especificacion
+        public string FormatearUnaFecha(DateTime laFecha,string elFormato)
+        {
+
+            return "";
+        }
+    }
+}
